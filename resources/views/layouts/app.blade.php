1577eb5b2f029a4d0b2ae5ed10f4cda06bfb1f05
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-02-06 13:29:11 +0530

    Try to fix erros but didn't work

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -389,7 +389,7 @@
     <div class="container">
       <div class="header-desk header-desk_type_1">
         <div class="logo">
-          <a href="{{ route('home.index')}}">
+          <a href="{{route('home.index')}}">
             <img src="{{ asset ('assets/images/logo.png') }}" alt="Uomo" class="logo__image d-block" />
           </a>
         </div>
@@ -509,7 +509,7 @@
       <div class="row row-cols-lg-5 row-cols-2">
         <div class="footer-column footer-store-info col-12 mb-4 mb-lg-0">
           <div class="logo">
-          <a href="{{ route('home.index')}}">
+          <a href="{{route('home.index')}}">
               <img src="{{ asset ('assets/images/logo.png') }}" alt="SurfsideMedia" class="logo__image d-block" />
             </a>
           </div>
