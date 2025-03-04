51de230c10a865a8153ad282b6ed341c688dfa5b
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-03-04 19:59:20 +0530

    Update the composer with intervention image pakage while creating the tumbline image og the Brands

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Brand;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Intervention\Image\Laravel\Facades\Image;
 
 class AdminController extends Controller
 {
@@ -20,8 +23,30 @@ class AdminController extends Controller
         return view('admin.brand-add');
     }
 
-    public function brand_store(){
-        
+    public function brand_store(Request $request){
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:brands,slug',
+            'image' => 'mimes:png,jpg,jpeg|max:2048'
+        ]);
+
+        $brand = new Brand();
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+
+        $image = $request->file('image');
+        $file_extention = $request->file('image')->extension();
+        $file_name = Carbon::now()->timestamp.'.'.$file_extention;
+
+        $brand ->image = $file_name;
+        $brand->save();
+        return redirect()->route('admin.brands')->with('status', 'Brand has been added succesfully');
+
+    }
+
+    public function GenarateBrandThumbailsImage($image, $imageName){
+        $destinationPath = public_path('uploads/brands');
+        $img = Image::read($image->path);
     }
 
 }
