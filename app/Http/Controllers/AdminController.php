9a368746e644be5a5250066fa557dfefdf611a5a
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-03-05 23:31:06 +0530

    Create Function and route for the Category Edit Function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -12,20 +12,24 @@ use Intervention\Image\Laravel\Facades\Image;
 
 class AdminController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('admin.index');
     }
 
-    public function brands() {
+    public function brands()
+    {
         $brands = Brand::orderBy('id', 'DESC')->paginate(10);
         return view('admin.brands', compact('brands'));
     }
 
-    public function add_brand(){
+    public function add_brand()
+    {
         return view('admin.brand-add');
     }
 
-    public function brand_store(Request $request){
+    public function brand_store(Request $request)
+    {
         $request->validate([
             'name' => 'required',
             'slug' => 'required|unique:brands,slug',
@@ -38,110 +42,117 @@ class AdminController extends Controller
 
         $image = $request->file('image');
         $file_extention = $request->file('image')->extension();
-        $file_name = Carbon::now()->timestamp.'.'.$file_extention;
+        $file_name = Carbon::now()->timestamp . '.' . $file_extention;
 
         $this->GenarateBrandThumbailsImage($image, $file_name);
 
-        $brand ->image = $file_name;
+        $brand->image = $file_name;
         $brand->save();
         return redirect()->route('admin.brands')->with('status', 'Brand has been added succesfully');
-
     }
 
-    public function brand_edit($id){
-    $brand = Brand::find($id);
-    return view('admin.brand-edit', compact('brand'));
-}
-
-    public function brand_update(Request $request){
-    $request->validate([
-        'name' => 'required',
-        'slug' => 'required|unique:brands,slug,' .$request->id,
-        'image' => 'mimes:png,jpg,jpeg|max:2048'
-    ]);
-
-    $brand = Brand::find($request->id);
-    $brand->name = $request->name;
-    $brand->slug = Str::slug($request->name);
-    if($request->hasFile('image'))
+    public function brand_edit($id)
     {
-        if  (File::exists(public_path('uploads/brands').'/'.$brand->image))
-        {
-            File::delete(public_path('uploads/brands').'/'.$brand->image);
+        $brand = Brand::find($id);
+        return view('admin.brand-edit', compact('brand'));
+    }
+
+    public function brand_update(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:brands,slug,' . $request->id,
+            'image' => 'mimes:png,jpg,jpeg|max:2048'
+        ]);
+
+        $brand = Brand::find($request->id);
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        if ($request->hasFile('image')) {
+            if (File::exists(public_path('uploads/brands') . '/' . $brand->image)) {
+                File::delete(public_path('uploads/brands') . '/' . $brand->image);
+            }
+            $image = $request->file('image');
+            $file_extention = $request->file('image')->extension();
+            $file_name = Carbon::now()->timestamp . '.' . $file_extention;
+
+            $this->GenarateBrandThumbailsImage($image, $file_name);
+
+            $brand->image = $file_name;
         }
-        $image = $request->file('image');
-        $file_extention = $request->file('image')->extension();
-        $file_name = Carbon::now()->timestamp.'.'.$file_extention;
 
-        $this->GenarateBrandThumbailsImage($image, $file_name);
 
-        $brand ->image = $file_name;
+        $brand->save();
+        return redirect()->route('admin.brands')->with('status', 'Brand has been updated succesfully');
     }
 
-
-    $brand->save();
-    return redirect()->route('admin.brands')->with('status', 'Brand has been updated succesfully');
-
-    }
-
-    public function GenarateBrandThumbailsImage($image, $imageName){
+    public function GenarateBrandThumbailsImage($image, $imageName)
+    {
         $destinationPath = public_path('uploads/brands');
         $img = Image::read($image->path());
-        $img->cover(124,124,"top");
-        $img->resize(124,124,function($constraint){
+        $img->cover(124, 124, "top");
+        $img->resize(124, 124, function ($constraint) {
             $constraint->aspectRatio();
-        })->save($destinationPath.'/'.$imageName);
+        })->save($destinationPath . '/' . $imageName);
     }
 
-    public function brand_delete($id){
+    public function brand_delete($id)
+    {
         $brand = Brand::find($id);
-        if  (File::exists(public_path('uploads/brands').'/'.$brand->image)){
-            File::delete(public_path('uploads/brands').'/'.$brand->image);
+        if (File::exists(public_path('uploads/brands') . '/' . $brand->image)) {
+            File::delete(public_path('uploads/brands') . '/' . $brand->image);
         }
         $brand->delete();
         return redirect()->route('admin.brands')->with('status', 'Brand has been deleted succesfully');
     }
 
 
-        public function categories(){
-            $categories = Category::orderBy('id', 'DESC')->paginate(10);
-            return view('admin.categories',compact('categories'));
-        }
+    public function categories()
+    {
+        $categories = Category::orderBy('id', 'DESC')->paginate(10);
+        return view('admin.categories', compact('categories'));
+    }
 
-        public function category_add(){
-            return view('admin.category-add');
-        }
+    public function category_add()
+    {
+        return view('admin.category-add');
+    }
 
-        public function category_store(Request $request){
-            $request->validate([
-                'name' => 'required',
-                'slug' => 'required|unique:categories,slug',
-                'image' => 'mimes:png,jpg,jpeg|max:2048'
-            ]);
-    
-            $category = new Category();
-            $category->name = $request->name;
-            $category->slug = Str::slug($request->name);
-    
-            $image = $request->file('image');
-            $file_extention = $request->file('image')->extension();
-            $file_name = Carbon::now()->timestamp.'.'.$file_extention;
-    
-            $this->GenarateCategoryThumbailsImage($image, $file_name);
-    
-            $category ->image = $file_name;
-            $category->save();
-            return redirect()->route('admin.categories')->with('status', 'Category has been added succesfully');
-    
-        }
+    public function category_store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'slug' => 'required|unique:categories,slug',
+            'image' => 'mimes:png,jpg,jpeg|max:2048'
+        ]);
 
-        public function GenarateCategoryThumbailsImage($image, $imageName){
-            $destinationPath = public_path('uploads/categories');
-            $img = Image::read($image->path());
-            $img->cover(124,124,"top");
-            $img->resize(124,124,function($constraint){
-                $constraint->aspectRatio();
-            })->save($destinationPath.'/'.$imageName);
-        }
+        $category = new Category();
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
 
-   }
+        $image = $request->file('image');
+        $file_extention = $request->file('image')->extension();
+        $file_name = Carbon::now()->timestamp . '.' . $file_extention;
+
+        $this->GenarateCategoryThumbailsImage($image, $file_name);
+
+        $category->image = $file_name;
+        $category->save();
+        return redirect()->route('admin.categories')->with('status', 'Category has been added succesfully');
+    }
+
+    public function GenarateCategoryThumbailsImage($image, $imageName)
+    {
+        $destinationPath = public_path('uploads/categories');
+        $img = Image::read($image->path());
+        $img->cover(124, 124, "top");
+        $img->resize(124, 124, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save($destinationPath . '/' . $imageName);
+    }
+
+    public function category_edit($id){
+        $category = Category::find($id);
+        return view('admin.category-edit',compact('category'));
+    }
+}
