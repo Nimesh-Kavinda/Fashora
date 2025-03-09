13536e049a3c9216f574c20855fa2e2da8b97068
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-03-09 11:45:20 +0530

    Create a Function for product add and connect them with category and brand

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -203,4 +203,12 @@ class AdminController extends Controller
         $products = Product::orderBy('created_at', 'DESC')->paginate(10);
         return view('admin.products',compact('products'));
     }
+
+    public function product_add()
+    {
+        $categories = Category::select('id', 'name')->orderBy('name')->get();
+        $brands = Brand::select('id', 'name')->orderBy('name')->get();
+        return view('admin.product-add',compact('categories','brands'));
+
+    }
 }
