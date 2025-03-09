eb063b07a9f784345f784ceb4e62ced1885b57f4
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-03-10 00:09:48 +0530

    Fix some Format Alings in add product page

--- resources/views/admin/products-add.blade.php
+++ resources/views/admin/products-add.blade.php
@@ -37,16 +37,14 @@
                 <fieldset class="name">
                     <div class="body-title mb-10">Product name <span class="tf-color-1">*</span>
                     </div>
-                    <input class="mb-10" type="text" placeholder="Enter product name"
-                        name="name" tabindex="0" value="" aria-required="true" required="">
+                    <input class="mb-10" type="text" placeholder="Enter product name" name="name" tabindex="0" value="" aria-required="true" required="">
                     <div class="text-tiny">Do not exceed 100 characters when entering the
                         product name.</div>
                 </fieldset>
 
                 <fieldset class="name">
                     <div class="body-title mb-10">Slug <span class="tf-color-1">*</span></div>
-                    <input class="mb-10" type="text" placeholder="Enter product slug"
-                        name="slug" tabindex="0" value="" aria-required="true" required="">
+                    <input class="mb-10" type="text" placeholder="Enter product slug" name="slug" tabindex="0" value="" aria-required="true" required="">
                     <div class="text-tiny">Do not exceed 100 characters when entering the
                         product name.</div>
                 </fieldset>
@@ -81,9 +79,7 @@
                 <fieldset class="shortdescription">
                     <div class="body-title mb-10">Short Description <span
                             class="tf-color-1">*</span></div>
-                    <textarea class="mb-10 ht-150" name="short_description"
-                        placeholder="Short Description" tabindex="0" aria-required="true"
-                        required=""></textarea>
+                    <textarea class="mb-10 ht-150" name="short_description" placeholder="Short Description" tabindex="0" aria-required="true" required=""></textarea>
                     <div class="text-tiny">Do not exceed 100 characters when entering the
                         product name.</div>
                 </fieldset>
@@ -91,8 +87,7 @@
                 <fieldset class="description">
                     <div class="body-title mb-10">Description <span class="tf-color-1">*</span>
                     </div>
-                    <textarea class="mb-10" name="description" placeholder="Description"
-                        tabindex="0" aria-required="true" required=""></textarea>
+                    <textarea class="mb-10" name="description" placeholder="Description" tabindex="0" aria-required="true" required=""></textarea>
                     <div class="text-tiny">Do not exceed 100 characters when entering the
                         product name.</div>
                 </fieldset>
@@ -103,16 +98,14 @@
                     </div>
                     <div class="upload-image flex-grow">
                         <div class="item" id="imgpreview" style="display:none">
-                            <img src="../../../localhost_8000/images/upload/upload-1.png"
-                                class="effect8" alt="">
+                            <img src="../../../localhost_8000/images/upload/upload-1.png" class="effect8" alt="">
                         </div>
                         <div id="upload-file" class="item up-load">
                             <label class="uploadfile" for="myFile">
                                 <span class="icon">
                                     <i class="icon-upload-cloud"></i>
                                 </span>
-                                <span class="body-text">Drop your images here or select <span
-                                        class="tf-color">click to browse</span></span>
+                                <span class="body-text">Drop your images here or select <span class="tf-color">click to browse</span></span>
                                 <input type="file" id="myFile" name="image" accept="image/*">
                             </label>
                         </div>
@@ -130,10 +123,8 @@
                                 <span class="icon">
                                     <i class="icon-upload-cloud"></i>
                                 </span>
-                                <span class="text-tiny">Drop your images here or select <span
-                                        class="tf-color">click to browse</span></span>
-                                <input type="file" id="gFile" name="images[]" accept="image/*"
-                                    multiple="">
+                                <span class="text-tiny">Drop your images here or select <span class="tf-color">click to browse</span></span>
+                                <input type="file" id="gFile" name="images[]" accept="image/*" multiple="">
                             </label>
                         </div>
                     </div>
@@ -141,18 +132,13 @@
 
                 <div class="cols gap22">
                     <fieldset class="name">
-                        <div class="body-title mb-10">Regular Price <span
-                                class="tf-color-1">*</span></div>
-                        <input class="mb-10" type="text" placeholder="Enter regular price"
-                            name="regular_price" tabindex="0" value="" aria-required="true"
-                            required="">
+                        <div class="body-title mb-10">Regular Price <span class="tf-color-1">*</span></div>
+                        <input class="mb-10" type="text" placeholder="Enter regular price" name="regular_price" tabindex="0" value="" aria-required="true" required="">
                     </fieldset>
                     <fieldset class="name">
                         <div class="body-title mb-10">Sale Price <span
                                 class="tf-color-1">*</span></div>
-                        <input class="mb-10" type="text" placeholder="Enter sale price"
-                            name="sale_price" tabindex="0" value="" aria-required="true"
-                            required="">
+                        <input class="mb-10" type="text" placeholder="Enter sale price" name="sale_price" tabindex="0" value="" aria-required="true" required="">
                     </fieldset>
                 </div>
 
@@ -161,15 +147,12 @@
                     <fieldset class="name">
                         <div class="body-title mb-10">SKU <span class="tf-color-1">*</span>
                         </div>
-                        <input class="mb-10" type="text" placeholder="Enter SKU" name="SKU"
-                            tabindex="0" value="" aria-required="true" required="">
+                        <input class="mb-10" type="text" placeholder="Enter SKU" name="SKU" tabindex="0" value="" aria-required="true" required="">
                     </fieldset>
                     <fieldset class="name">
                         <div class="body-title mb-10">Quantity <span class="tf-color-1">*</span>
                         </div>
-                        <input class="mb-10" type="text" placeholder="Enter quantity"
-                            name="quantity" tabindex="0" value="" aria-required="true"
-                            required="">
+                        <input class="mb-10" type="text" placeholder="Enter quantity" name="quantity" tabindex="0" value="" aria-required="true" required="">
                     </fieldset>
                 </div>
 
