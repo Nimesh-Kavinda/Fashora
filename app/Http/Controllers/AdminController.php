8b8fb6b267b8f142264335999b33f165f1918660
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-03-10 00:28:46 +0530

    Start to Create Product Store function

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -208,7 +208,12 @@ class AdminController extends Controller
     {
         $categories = Category::select('id', 'name')->orderBy('name')->get();
         $brands = Brand::select('id', 'name')->orderBy('name')->get();
-        return view('admin.product-add',compact('categories','brands'));
+        return view('admin.products-add',compact('categories','brands'));
 
+    }
+
+    public function product_store(Request $request)
+    {
+        
     }
 }
