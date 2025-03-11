94b5acbb2522839b40bcd54db091e90ebc5f6473
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-03-11 21:38:39 +0530

    Start to test product add function

--- resources/views/admin/products.blade.php
+++ resources/views/admin/products.blade.php
@@ -39,7 +39,7 @@
             <div class="table-responsive">
                 @if (Session::has('status'))
                 <p class="alert alert-success">{{Session::get('status')}}</p>
-                @endif
+                @endif 
                 <table class="table table-striped table-bordered">
                     <thead>
                         <tr>
