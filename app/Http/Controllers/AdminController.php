8dd824ace903f1d7e2c4facb7896ef1574ad4ebb
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-03-12 21:57:02 +0530

    Created Function for Update Product

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -326,6 +326,76 @@ class AdminController extends Controller
             'brand_id' => 'required'
         ]);
 
-        
+        $product = Product::find($request->id);
+        $product->name = $request->name;
+        $product->slug = Str::slug($request->name);
+        $product->short_description = $request->short_description;
+        $product->description = $request->description;
+        $product->regular_price = $request->regular_price;
+        $product->sale_price = $request->sale_price;
+        $product->SKU = $request->SKU;
+        $product->stock_status = $request->stock_status;
+        $product->featured = $request->featured;
+        $product->quantity = $request->quantity;
+        $product->category_id = $request->category_id;
+        $product->brand_id = $request->brand_id;
+
+        $current_timestamp = Carbon::now()->timestamp;
+
+        if ($request->hasFile('image')) 
+        {   
+            if(File::exists(public_path('uploads/products').'/'.$product->image))
+            {
+                File::delete(public_path('uploads/products').'/'.$product->image);
+            }
+            if(File::exists(public_path('uploads/products/thumbnails').'/'.$product->image))
+            {
+                File::delete(public_path('uploads/products/thumbnails').'/'.$product->image);
+            }
+            $image = $request->file('image');
+            $imageName = $current_timestamp . '.' . $image->extension();
+            $this -> GenerateProductThumbnailImage($image, $imageName);
+            $product->image = $imageName;
+        }
+
+        $gallery_arr = array();
+        $gallery_images = "";
+        $counter = 1;
+
+        if($request->hasFile('images'))
+        {   
+            foreach(explode(',',$product->images) as $ofile)
+            {
+                if(File::exists(public_path('uploads/products').'/'.$ofile))
+                {
+                    File::delete(public_path('uploads/products').'/'.$ofile);
+                }
+                if(File::exists(public_path('uploads/products/thumbnails').'/'.$ofile))
+                {
+                    File::delete(public_path('uploads/products/thumbnails').'/'.$ofile);
+                }
+            }
+
+            $allowedfileExtension = ['jpg', 'png', 'jpeg'];
+            $files = $request->file('images');
+
+            foreach($files as $file)
+            {
+                $gextension = $file->getClientOriginalExtension();
+                $gcheck = in_array($gextension, $allowedfileExtension);
+
+                if($gcheck)
+                {
+                    $gfileName = $current_timestamp. "-" . $counter . "." .$gextension;
+                    $this->GenerateProductThumbnailImage($file, $gfileName);
+                    array_push($gallery_arr,$gfileName);
+                    $counter = $counter + 1;
+                }
+            }
+            $gallery_images = implode(',', $gallery_arr);
+        }
+        $product->images = $gallery_images;
+        $product->save();
+        return redirect()->route('admin.products')->with('status', 'Product has been Updated successfully.!');
     }
 }
