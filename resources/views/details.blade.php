713493d14198919ba6398e4e4e9d311e7130246f
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-03-17 20:56:53 +0530

    Conplete add to cart function and testing

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -431,7 +431,19 @@
                     alt="{{ $gimg }}" class="pc__img pc__img-second">
                     @endforeach
                 </a>
-                <button class="pc__atc btn anim_appear-bottom btn position-absolute border-0 text-uppercase fw-medium js-add-cart js-open-aside" data-aside="cartDrawer" title="Add To Cart">Add To Cart</button>
+                @if(Cart::instance('cart')->content()->where('id',$rproduct->id)->count()>0)
+                <a href="{{ route('cart.index') }}" class="pc__atc btn anim_appear-bottom btn position-absolute border-0 text-uppercase fw-medium btn-warning mb-3">Go to Cart</a>
+                @else
+
+                <form name="addtocart-form" method="post" action="{{ route('cart.add') }}">
+                  @csrf
+                  <input type="hidden" name="id" value="{{ $rproduct->id }}">
+                  <input type="hidden" name="quantity" value="1">
+                  <input type="hidden" name="name" value="{{ $rproduct->name }}">
+                  <input type="hidden" name="price" value="{{ $rproduct->sale_price == '' ? $rproduct->regular_price : $rproduct->sale_price }}">
+                <button type="submit" class="pc__atc btn anim_appear-bottom btn position-absolute border-0 text-uppercase fw-medium" data-aside="cartDrawer" title="Add To Cart">Add To Cart</button>
+
+                @endif
               </div>
 
               <div class="pc__info position-relative">
