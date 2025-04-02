e4eacfdff71572d1ecf08c7ede4456a313bae368
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-04-02 22:46:19 +0530

    Create route for the wishlist add function in web.php

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use GuzzleHttp\Psr7\Request as Psr7Request;
 use Illuminate\Http\Request;
-
+use Surfsidemedia\Shoppingcart\Facades\Cart;
 
 class WishlistController extends Controller
 {
     public function add_to_wishlist(Request $request)
     {
-        Cart::instance('wishlist')->add($request->id,$request->name,$request->quantity);
+        Cart::instance('wishlist')->add($request->id,$request->name,$request->quantity,$request->price)->associate('App\Models\Product');
+        return redirect()->back();
     }
 }
