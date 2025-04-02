3e28a231bc7971bed553cabaa2f88f063243a445
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-04-03 00:23:55 +0530

    Changed Product Details page other item wishlist icon colourd and also set the functionality

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -435,12 +435,45 @@
                   </span>
                 </div>
 
-                <button class="pc__btn-wl position-absolute top-0 end-0 bg-transparent border-0 js-add-wishlist"
+                @guest
+
+                <a href="{{ route('login') }}"><button class="pc__btn-wl position-absolute top-0 end-0 bg-transparent border-0 js-add-wishlist"
+                  title="Add To Wishlist">
+                  <svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <use href="#icon_heart" />
+                  </svg>
+                </button></a>
+
+                @else
+
+                @if(Cart::instance('wishlist')->content()->where('id', $product->id)->count() > 0)
+
+                <button type="submit" disabled class="pc__btn-wl position-absolute top-0 end-0 bg-transparent border-0 js-add-wishlist filled-heart"
+                title="Add To Wishlist">
+                <svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <use href="#icon_heart" />
+                </svg>
+              </button>
+
+                @else
+
+                <form action="{{ route('wishlist.add') }}" method="POST">
+                  @csrf
+                  <input type="hidden" name="id" value="{{ $product->id }}">
+                  <input type="hidden" name="name" value="{{ $product->name }}">
+                  <input type="hidden" name="price" value="{{ $product->sale_price == '' ? $product->regular_price : $product->sale_price }}">
+                  <input type="hidden" name="quantity" value="1">
+                  <button type="submit" class="pc__btn-wl position-absolute top-0 end-0 bg-transparent border-0 js-add-wishlist"
                   title="Add To Wishlist">
                   <svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <use href="#icon_heart" />
                   </svg>
                 </button>
+            </form>
+
+            @endif
+
+              @endguest
 
               </div>
             </div>
