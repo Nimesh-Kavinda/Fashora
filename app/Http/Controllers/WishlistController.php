0844dd60bcb9c1ece0488b2f9bc641f0ce51253c
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-04-02 22:32:28 +0530

    Start to make add Product to wishlist function

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Psr7\Request as Psr7Request;
 use Illuminate\Http\Request;
+
 
 class WishlistController extends Controller
 {
-                   
+    public function add_to_wishlist(Request $request)
+    {
+        Cart::instance('wishlist')->add($request->id,$request->name,$request->quantity);
+    }
 }
