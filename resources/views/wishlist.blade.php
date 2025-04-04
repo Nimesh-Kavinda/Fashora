d567b633d9d3ebf923e8c37744b4fb60ad86f069
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-04-04 21:53:10 +0530

    Removed some unnesaary contents from wishlist page

--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -6,29 +6,7 @@
     <div class="mb-4 pb-4"></div>
     <section class="shop-checkout container">
       <h2 class="page-title">Wishlist</h2>
-      {{-- <div class="checkout-steps">
-        <a href="shop_cart.html" class="checkout-steps__item active">
-          <span class="checkout-steps__item-number">01</span>
-          <span class="checkout-steps__item-title">
-            <span>Shopping Bag</span>
-            <em>Manage Your Items List</em>
-          </span>
-        </a>
-        <a href="shop_checkout.html" class="checkout-steps__item">
-          <span class="checkout-steps__item-number">02</span>
-          <span class="checkout-steps__item-title">
-            <span>Shipping and Checkout</span>
-            <em>Checkout Your Items List</em>
-          </span>
-        </a>
-        <a href="shop_order_complete.html" class="checkout-steps__item">
-          <span class="checkout-steps__item-number">03</span>
-          <span class="checkout-steps__item-title">
-            <span>Confirmation</span>
-            <em>Review And Submit Your Order</em>
-          </span>
-        </a>
-      </div> --}}
+
       <div class="shopping-cart">
         <div class="cart-table__wrapper">
           <table class="cart-table">
@@ -165,57 +143,7 @@
             <button class="btn btn-light">UPDATE CART</button>
           </div>
         </div>
-        <div class="shopping-cart__totals-wrapper">
-          <div class="sticky-content">
-            <div class="shopping-cart__totals">
-              <h3>Cart Totals</h3>
-              <table class="cart-totals">
-                <tbody>
-                  <tr>
-                    <th>Subtotal</th>
-                    <td>$1300</td>
-                  </tr>
-                  <tr>
-                    <th>Shipping</th>
-                    <td>
-                      <div class="form-check">
-                        <input class="form-check-input form-check-input_fill" type="checkbox" value=""
-                          id="free_shipping">
-                        <label class="form-check-label" for="free_shipping">Free shipping</label>
-                      </div>
-                      <div class="form-check">
-                        <input class="form-check-input form-check-input_fill" type="checkbox" value="" id="flat_rate">
-                        <label class="form-check-label" for="flat_rate">Flat rate: $49</label>
-                      </div>
-                      <div class="form-check">
-                        <input class="form-check-input form-check-input_fill" type="checkbox" value=""
-                          id="local_pickup">
-                        <label class="form-check-label" for="local_pickup">Local pickup: $8</label>
-                      </div>
-                      <div>Shipping to AL.</div>
-                      <div>
-                        <a href="#" class="menu-link menu-link_us-s">CHANGE ADDRESS</a>
-                      </div>
-                    </td>
-                  </tr>
-                  <tr>
-                    <th>VAT</th>
-                    <td>$19</td>
-                  </tr>
-                  <tr>
-                    <th>Total</th>
-                    <td>$1319</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div class="mobile_fixed-btn_wrapper">
-              <div class="button-wrapper container">
-                <button class="btn btn-primary btn-checkout">PROCEED TO CHECKOUT</button>
-              </div>
-            </div>
-          </div>
-        </div>
+
       </div>
     </section>
   </main>
