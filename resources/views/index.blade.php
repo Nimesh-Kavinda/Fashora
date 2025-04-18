92e0e74ffdb2b4fedae27ce761cbaebfe101f17a
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-04-18 20:20:05 +0530

    changed some responsive issues

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -868,7 +868,7 @@
                 <svg width="16" height="16" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <use href="#icon_heart" />
                 </svg>            
-              </button>
+              </button>   
             </div>                
           </div>
         </div>
