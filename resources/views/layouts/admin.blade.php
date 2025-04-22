8c2e520322208a9bfb11c1651d30da4be195bd2c
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-04-22 21:54:22 +0530

    Add a comment to crsf tokens that added manually

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,8 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-
-    
+                  
+    {{-- crsf tokens added manuallay --}}
     <meta name="csrf-token" content="{{ csrf_token() }}">          
 
     <title>{{ config('app.name', 'Laravel') }}</title>
