3e2891fd6eb3623ae9150298ce7a1f1acba63391
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-04-24 00:44:02 +0530

    Finsih Coupon Adding function and fetch them in to a tabel in Coupon page

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -441,14 +441,23 @@ class AdminController extends Controller
     public function coupon_store(Request $request)
     {
         $request->validate([
-            'code' => 'requried',
-            'type' => 'requried',
-            'value' => 'requried|numeric',
-            'cart_value' => 'requried|numeric',
-            'expiry_date' => 'requried|date',
+            'code' => 'required',
+            'type' => 'required',
+            'value' => 'required|numeric',
+            'cart_value' => 'required|numeric',
+            'expiry_date' => 'required|date'
         ]);
 
+        $coupon = new Coupon();
+        $coupon->code = $request->code; 
+        $coupon->type = $request->type; 
+        $coupon->value = $request->value; 
+        $coupon->cart_value = $request->cart_value; 
+        $coupon->expiry_date = $request->expiry_date;
         
+        $coupon->save();
+
+        return redirect()->route('admin.coupons')->with('status', 'Coupon has been added successfully');
     }
 
 }
