340cdd550e3389649a9742a0648400c67ba72628
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-04-24 22:47:08 +0530

    Create Coupon Remove Function

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -117,11 +117,12 @@ class CartController extends Controller
             ]);
         }
 
-        else
-
-        {
-
-        }
     }
 
+    public function remove_coupon_code()
+    {
+        Session::forget('coupon');
+        Session::forget('discounts');
+        return back()->with('success', 'Coupon has been removed!');
+    }
 }
