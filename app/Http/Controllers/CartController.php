991ac3b97ca8ba5ae1888482a200d98d4d0f79af
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-04-25 20:00:12 +0530

    Create Route for Checkout and Made the View For checkout page

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Address;
 use App\Models\Coupon;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Surfsidemedia\Shoppingcart\Facades\Cart;
 
@@ -124,5 +126,17 @@ class CartController extends Controller
         Session::forget('coupon');
         Session::forget('discounts');
         return back()->with('success', 'Coupon has been removed!');
+    }
+
+    public function checkout()
+    {
+        if(!Auth::check())
+        {
+            return redirect()->route('login');
+        }
+
+        $address = Address::where('user_id',Auth::user()->id)->where('isdefault',1)->first();
+        return view('checkout',compact('address'));
+
     }
 }
