24ee653d0edc0a916e67aa3c3eb025664488f051
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-04-25 18:49:24 +0530

    Make Relationships Among Model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -13,4 +13,14 @@ class Order extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+
+    public function transaction()
+    {
+        return $this->hasOne(Transaction::class);
+    }
 }
