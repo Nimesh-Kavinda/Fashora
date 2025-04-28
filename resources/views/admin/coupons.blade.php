85298cfa3751ce3e4b94d163f6fdfda2cdb6b29a
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-04-28 22:06:35 +0530

    Chnage the currncy and add % mark in admin layout

--- resources/views/admin/coupons.blade.php
+++ resources/views/admin/coupons.blade.php
@@ -64,8 +64,14 @@
                                     <td>{{ $coupon->id }}</td>
                                     <td>{{ $coupon->code }}</td>
                                     <td>{{ $coupon->type }}</td>
-                                    <td>${{ $coupon->value }}</td>
-                                    <td>${{ $coupon->cart_value }}</td>
+                                    <td>
+                                        @if($coupon->type == 'fixed')
+                                            Rs {{ $coupon->value }}
+                                        @else
+                                            {{ $coupon->value }} %
+                                        @endif
+                                    </td>
+                                    <td>Rs {{ $coupon->cart_value }}</td>
                                     <td>{{ $coupon->expiry_date }}</td>
                                     <td>
                                         <div class="list-icon-function">
