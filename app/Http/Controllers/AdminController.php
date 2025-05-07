a1ce9da39620cdd33dfeea1c8e360d5cdf77f1c3
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-05-08 00:10:42 +0530

    Create DB query to Get details Form Months tabel with Orders

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -34,7 +34,28 @@ class AdminController extends Controller
                                         sum(if(status='canceled', 1,0)) As TotalCanceled
                                         From Orders
                                  ");
-
+        $monthlyDatas = DB::select("SELECT 
+                                        M.id AS MonthNo, 
+                                        M.name AS MonthName,
+                                        IFNULL(D.TotalAmount, 0) AS TotalAmount,
+                                        IFNULL(D.TotalOrderedAmount, 0) AS TotalOrderedAmount,
+                                        IFNULL(D.TotalDeliveredAmount, 0) AS TotalDeliveredAmount,
+                                        IFNULL(D.TotalCanceledAmount, 0) AS TotalCanceledAmount 
+                                    FROM month_names M
+                                    LEFT JOIN (
+                                        SELECT 
+                                            DATE_FORMAT(created_at, '%b') AS MonthName,
+                                            MONTH(created_at) AS MonthNo,
+                                            SUM(total) AS TotalAmount,
+                                            SUM(IF(status = 'ordered', total, 0)) AS TotalOrderedAmount,
+                                            SUM(IF(status = 'delivered', total, 0)) AS TotalDeliveredAmount,
+                                            SUM(IF(status = 'canceled', total, 0)) AS TotalCanceledAmount
+                                        FROM Orders 
+                                        WHERE YEAR(created_at) = YEAR(NOW()) 
+                                        GROUP BY YEAR(created_at), MONTH(created_at), DATE_FORMAT(created_at, '%b')
+                                        ORDER BY MONTH(created_at)
+                                    ) D ON D.MonthNo = M.id;
+                                    ");
         
         return view('admin.index', compact('orders','dashboardDatas'));
     }
