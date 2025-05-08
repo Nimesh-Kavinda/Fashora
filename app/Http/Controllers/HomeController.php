688edd958accb0881167124239ed3cef68ed7618
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-05-08 09:41:35 +0530

    Make Function to Show Onsale Product in Home Page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Slide;
 use Illuminate\Http\Request;
 
@@ -9,8 +10,9 @@ class HomeController extends Controller
 {
    
     public function index()
-    {
+    {   
+        $categories = Category::orderBy('name')->get();
         $slides = Slide::where('status', 1)->get()->take(4);
-        return view('index', compact('slides'));
+        return view('index', compact('slides','categories'));
     }
 }
