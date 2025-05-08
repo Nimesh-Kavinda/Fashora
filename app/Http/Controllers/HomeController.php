8f7106fed7e38af66cfd4fc3daf3f59d1fa35589
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-05-08 10:08:56 +0530

    Fectch Categories tot the Home page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Product;
 use App\Models\Slide;
 use Illuminate\Http\Request;
 
@@ -12,7 +13,8 @@ class HomeController extends Controller
     public function index()
     {   
         $categories = Category::orderBy('name')->get();
+        $sproducts = Product::whereNotNull('sale_price')->where('sale_price','<>','')->inRandomOrder()->get()->take(8);
         $slides = Slide::where('status', 1)->get()->take(4);
-        return view('index', compact('slides','categories'));
+        return view('index', compact('slides','categories','sproducts'));
     }
 }
