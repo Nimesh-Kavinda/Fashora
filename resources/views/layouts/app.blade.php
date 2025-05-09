2fa3cdd7b9b1a3a31fae000b9634158625ff4128
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-05-09 20:04:03 +0530

    Make some chnages in the footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -634,46 +634,46 @@
         <div class="footer-column footer-menu mb-4 mb-lg-0">
           <h6 class="sub-menu__title text-uppercase">Company</h6>
           <ul class="sub-menu__list list-unstyled">
-            <li class="sub-menu__item"><a href="about-2.html" class="menu-link menu-link_us-s">About Us</a></li>
-            <li class="sub-menu__item"><a href="#" class="menu-link menu-link_us-s">Careers</a></li>
-            <li class="sub-menu__item"><a href="#" class="menu-link menu-link_us-s">Affiliates</a></li>
-            <li class="sub-menu__item"><a href="blog_list1.html" class="menu-link menu-link_us-s">Blog</a></li>
-            <li class="sub-menu__item"><a href="contact-2.html" class="menu-link menu-link_us-s">Contact Us</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">About Us</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">Careers</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">Affiliates</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">Blog</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">Contact Us</a></li>
           </ul>
         </div>
 
         <div class="footer-column footer-menu mb-4 mb-lg-0">
           <h6 class="sub-menu__title text-uppercase">Shop</h6>
           <ul class="sub-menu__list list-unstyled">
-            <li class="sub-menu__item"><a href="shop2.html" class="menu-link menu-link_us-s">New Arrivals</a></li>
-            <li class="sub-menu__item"><a href="shop3.html" class="menu-link menu-link_us-s">Accessories</a></li>
-            <li class="sub-menu__item"><a href="shop4.html" class="menu-link menu-link_us-s">Men</a></li>
-            <li class="sub-menu__item"><a href="shop5.html" class="menu-link menu-link_us-s">Women</a></li>
-            <li class="sub-menu__item"><a href="shop1.html" class="menu-link menu-link_us-s">Shop All</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">New Arrivals</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">Accessories</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">Men</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">Women</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">Shop All</a></li>
           </ul>
         </div>
 
         <div class="footer-column footer-menu mb-4 mb-lg-0">
           <h6 class="sub-menu__title text-uppercase">Help</h6>
           <ul class="sub-menu__list list-unstyled">
-            <li class="sub-menu__item"><a href="#" class="menu-link menu-link_us-s">Customer Service</a></li>
-            <li class="sub-menu__item"><a href="account_dashboard.html" class="menu-link menu-link_us-s">My Account</a>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">Customer Service</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">My Account</a>
             </li>
-            <li class="sub-menu__item"><a href="store_location.html" class="menu-link menu-link_us-s">Find a Store</a>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">Find a Store</a>
             </li>
-            <li class="sub-menu__item"><a href="#" class="menu-link menu-link_us-s">Legal & Privacy</a></li>
-            <li class="sub-menu__item"><a href="#" class="menu-link menu-link_us-s">Gift Card</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">Legal & Privacy</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">Gift Card</a></li>
           </ul>
         </div>
 
         <div class="footer-column footer-menu mb-4 mb-lg-0">
           <h6 class="sub-menu__title text-uppercase">Categories</h6>
           <ul class="sub-menu__list list-unstyled">
-            <li class="sub-menu__item"><a href="#" class="menu-link menu-link_us-s">Shirts</a></li>
-            <li class="sub-menu__item"><a href="#" class="menu-link menu-link_us-s">Jeans</a></li>
-            <li class="sub-menu__item"><a href="#" class="menu-link menu-link_us-s">Shoes</a></li>
-            <li class="sub-menu__item"><a href="#" class="menu-link menu-link_us-s">Bags</a></li>
-            <li class="sub-menu__item"><a href="#" class="menu-link menu-link_us-s">Shop All</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">Shirts</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">Jeans</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">Shoes</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">Bags</a></li>
+            <li class="sub-menu__item"><a href="javascript:void(0)" class="menu-link menu-link_us-s">Shop All</a></li>
           </ul>
         </div>
       </div>
