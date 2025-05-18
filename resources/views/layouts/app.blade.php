a9371091ac5a668d0686e06acc46cb6d5ba9aa18
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-05-18 23:59:27 +0530

    aling the footer logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -631,7 +631,7 @@
       <div class="row row-cols-lg-5 row-cols-2">
         <div class="footer-column footer-store-info col-12 mb-4 mb-lg-0">
           <div class="logo">
-            <a href="{{route('home.index')}}">
+            <a href="{{route('home.index')}}" class="text-center">
               <img src="{{ asset('assets/images/logo.png') }}" alt="SurfsideMedia" class="logo__image_2 d-block" />
             </a>
           </div>
