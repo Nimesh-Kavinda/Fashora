cb1fcdcbda775e5bb4afedb137dc340a3d097f84
Nimesh-Kavinda <nimeshkavindakarunasinghe@gmail.com>
2025-06-04 22:32:51 +0530

    Enhance checkout form inputs with autocomplete attribute for improved user experience

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    
+
 <main class="pt-90">
     <div class="mb-4 pb-4"></div>
     <section class="shop-checkout container">
@@ -55,7 +55,7 @@
                           <p>{{ $address->zip }}.</p>
                           <br>
                           <p>Contact Number -: {{ $address->phone }}</p>
-                        </div> 
+                        </div>
                     </div>
                  </div>
               </div>
@@ -65,56 +65,56 @@
             <div class="row mt-5">
               <div class="col-md-11">
                 <div class="form-floating my-3">
-                  <input type="text" class="form-control" name="name" required="" value="{{ old('name') }}">
+                  <input type="text" class="form-control" name="name" required="" value="{{ old('name') }}" autocomplete="off">
                   <label for="name">Full Name *</label>
                 @error('name')  <span class="text-danger">{{ $message }}</span> @enderror
                 </div>
               </div>
               <div class="col-md-8">
                 <div class="form-floating my-3">
-                  <input type="text" class="form-control" name="phone" required="" value="{{ old('phone') }}">
+                  <input type="text" class="form-control" name="phone" required="" value="{{ old('phone') }}" autocomplete="off">
                   <label for="phone">Phone Number *</label>
                   @error('phone')  <span class="text-danger">{{ $message }}</span> @enderror
                 </div>
               </div>
               <div class="col-md-7">
                 <div class="form-floating my-3">
-                  <input type="text" class="form-control" name="zip" required="" value="{{ old('zip') }}">
+                  <input type="text" class="form-control" name="zip" required="" value="{{ old('zip') }}" autocomplete="off">
                   <label for="zip">Pincode *</label>
                   @error('zip')  <span class="text-danger">{{ $message }}</span> @enderror
                 </div>
               </div>
               <div class="col-md-6">
                 <div class="form-floating mt-3 mb-3">
-                  <input type="text" class="form-control" name="state" required="" value="{{ old('state') }}">
+                  <input type="text" class="form-control" name="state" required="" value="{{ old('state') }}" autocomplete="off">
                   <label for="state">State *</label>
                   @error('state')  <span class="text-danger">{{ $message }}</span> @enderror
                 </div>
               </div>
               <div class="col-md-5">
                 <div class="form-floating my-3">
-                  <input type="text" class="form-control" name="city" required="" value="{{ old('city') }}">
+                  <input type="text" class="form-control" name="city" required="" value="{{ old('city') }}" autocomplete="off">
                   <label for="city">Town / City *</label>
                   @error('city')  <span class="text-danger">{{ $message }}</span> @enderror
                 </div>
               </div>
               <div class="col-md-6">
                 <div class="form-floating my-3">
-                  <input type="text" class="form-control" name="address" required="" value="{{ old('address') }}">
+                  <input type="text" class="form-control" name="address" required="" value="{{ old('address') }}" autocomplete="off">
                   <label for="address">House no, Building Name *</label>
                   @error('address')  <span class="text-danger">{{ $message }}</span> @enderror
                 </div>
               </div>
               <div class="col-md-5">
                 <div class="form-floating my-3">
-                  <input type="text" class="form-control" name="locality" required="" value="{{ old('locality') }}">
+                  <input type="text" class="form-control" name="locality" required="" value="{{ old('locality') }}" autocomplete="off">
                   <label for="locality">Road Name, Area, Colony *</label>
                   @error('locality')  <span class="text-danger">{{ $message }}</span> @enderror
                 </div>
               </div>
               <div class="col-md-12">
                 <div class="form-floating my-3">
-                  <input type="text" class="form-control" name="landmark" required="" value="{{ old('landmark') }}">
+                  <input type="text" class="form-control" name="landmark" required="" value="{{ old('landmark') }}" autocomplete="off">
                   <label for="landmark">Landmark *</label>
                   @error('landmark')  <span class="text-danger">{{ $message }}</span> @enderror
                 </div>
@@ -232,7 +232,7 @@
                     policy</a>.
                 </div>
               </div>
-              
+
               <button class="btn btn-primary btn-checkout">PLACE ORDER</button>
             </div>
           </div>
@@ -241,7 +241,7 @@
     </section>
   </main>
 
- 
+
 @push('scripts')
 <script>
      $(function () {
